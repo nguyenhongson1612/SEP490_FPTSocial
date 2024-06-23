c389a0e7a57fafc11fcc68d155019dbd44616f08
Khouse169 <nguyenkhaihoandtd@gmail.com>
2024-06-23 16:55:02 +0700

    Create post update

---
+++ Server/fptsocial/Application/Commands/CreateUserPost/CreateUserPostCommand.cs
@@ -0,0 +1,22 @@
+﻿using Core.CQRS.Command;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Application.Commands.Post
+{
+    public class CreateUserPostCommand : ICommand<CreateUserPostCommandResult>
+    {
+       public Guid UserId { get; set; }
+       public string Content { get; set; }
+       public Guid UserStatusId { get; set; }
+       public string UserPostNumber { get; set; }
+        public IEnumerable<string>? Photos { get; set; }
+        public IEnumerable<string>? Videos { get; set; }
+
+    }
+}
---
+++ Server/fptsocial/Application/Commands/CreateUserPost/CreateUserPostCommandResult.cs
@@ -0,0 +1,33 @@
+﻿using Domain.CommandModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Post
+{
+    public class CreateUserPostCommandResult
+    {
+        public string UserPostId { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public string? Content { get; set; }
+        public string? UserPostNumber { get; set; }
+        public string UserStatusId { get; set; } = null!;
+        public bool? IsAvataPost { get; set; }
+        public bool? IsCoverPhotoPost { get; set; }
+        public bool? IsHide { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual Domain.CommandModels.UserProfile User { get; set; } = null!;
+        public virtual UserStatus UserStatus { get; set; } = null!;
+        public virtual ICollection<CommentPost> CommentPosts { get; set; }
+        public virtual ICollection<ReactComment> ReactComments { get; set; }
+        public virtual ICollection<ReactPost> ReactPosts { get; set; }
+        public virtual ICollection<ReportPost> ReportPosts { get; set; }
+        public virtual ICollection<SharePost> SharePosts { get; set; }
+        public virtual ICollection<UserPostPhoto> UserPostPhotos { get; set; }
+        public virtual ICollection<UserPostVideo> UserPostVideos { get; set; }
+    }
+}
