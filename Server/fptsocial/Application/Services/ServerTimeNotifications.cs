c824943916146c8d870324a15873e34c61be8576
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-24 14:01:33 +0700

    Add base signalR to fix conflic folder

--- Server/fptsocial/Application/Services/ServerTimeNotifications.cs
+++ Server/fptsocial/Application/Services/ServerTimeNotifications.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Application.Hub;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
