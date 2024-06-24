6aefc2d241f16229fd8a2ccf0ea3c1f9dedd0ac5
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-24 14:01:33 +0700

    Add base signalR to fix conflic folder

---
+++ Server/fptsocial/API/Hub/NotificationsHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Hub;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Hub
+{
+    public class NotificationsHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        private readonly IConfiguration _configuration;
+        public NotificationsHub(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public override async Task OnConnectedAsync()
+        {
+
+            //await Clients.Client(Context.ConnectionId).ReceiveNotification($"The {Context.User?.Identity?.Name} ({Context.ConnectionId}) connected success!");
+            await Clients.All.SendAsync("ReceiveMessage",$"The ({Context.ConnectionId}) connected success!");
+            base.OnConnectedAsync();
+
+        }
+
+        public async Task SendNotifications(string message)
+        {
+            //await Clients.Client(Context.ConnectionId).ReceiveNotification(message);
+            //await Clients.All.ReceiveNotification($"{Context.ConnectionId} :{message}");
+        }
+
+
+
+    }
+
+}
---
+++ Server/fptsocial/Application/Services/ServerTimeNotifications.cs
@@ -0,0 +1,38 @@
+﻿using Application.Hub;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    //public class ServerTimeNotifications : BackgroundService
+    //{
+    //    private static readonly TimeSpan Period = TimeSpan.FromSeconds(2); //--> Frequency To Send Noti
+    //    private readonly ILogger<ServerTimeNotifications> _logger;
+    //    private readonly IHubContext<NotificationsHub, INotificationsClient> _context;
+
+    //    public ServerTimeNotifications(ILogger<ServerTimeNotifications> logger, IHubContext<NotificationsHub, INotificationsClient> context)
+    //    {
+    //        _logger = logger;
+    //        _context = context;
+    //    }
+
+    //    protected override async task executeasync(cancellationtoken stoppingtoken)
+    //    {
+    //        using periodictimer timer = new periodictimer(period);
+    //        while (!stoppingtoken.iscancellationrequested && await timer.waitfornexttickasync(stoppingtoken))
+    //        {
+    //            datetime datetime = datetime.now;
+    //            _logger.loginformation($"excute {1}", nameof(servertimenotifications), datetime);
+
+    //            await _context.clients.all.receivenotification($"server time = {datetime}");
+    //        }
+
+    //    }
+    //}
+}
