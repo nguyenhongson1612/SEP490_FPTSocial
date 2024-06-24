f652cf2d60ad2e6a283c2979d2aef7b4ad68dbc8
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-24 14:01:33 +0700

    Add base signalR to fix conflic folder

---
+++ Server/fptsocial/API/Hub/INotificationsClient.cs
@@ -0,0 +1,18 @@
+﻿using Domain.CommandModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Hub
+{
+    public interface INotificationsClient
+    {
+        Task ReceiveNotification(string message);
+        Task ReceiveNotification(string firstMessage, string secondMessage);
+        Task ReceiveNotification(string events, string firstMessage, string secondMessage);
+        Task ReceiveNotification(string events,UserProfile userProfile, string firstMessage, string secondMessage);
+        Task ReceiveNotification(object events, UserProfile userProfile, string firstMessage, string secondMessage);
+    }
+}
