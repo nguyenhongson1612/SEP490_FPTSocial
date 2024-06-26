e6d348fdd571a040941315630348683365090c0a
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-26 22:20:26 +0700

    Updare ConnectMapping and SignalR config

--- Server/fptsocial/API/Hub/NotificationsHub.cs
+++ Server/fptsocial/API/Hub/NotificationsHub.cs
@@ -3,34 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using API.Hub;
 using Application.Hub;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
 
 namespace Application.Hub
 {
-    public class NotificationsHub : Microsoft.AspNetCore.SignalR.Hub
+    public class NotificationsHub : Hub<INotificationsClient>
     {
-        private readonly IConfiguration _configuration;
+        private readonly static ConnectionMapping<string> _connections =
+            new ConnectionMapping<string>();
+        
+        private readonly IConfiguration _configuration; 
+        //private readonly HubConnection _hubConnection;
+
         public NotificationsHub(IConfiguration configuration)
         {
             _configuration = configuration;
+            //_hubConnection = hubConnection;
         }
         public override async Task OnConnectedAsync()
         {
-
-            //await Clients.Client(Context.ConnectionId).ReceiveNotification($"The {Context.User?.Identity?.Name} ({Context.ConnectionId}) connected success!");
-            await Clients.All.SendAsync("ReceiveMessage",$"The ({Context.ConnectionId}) connected success!");
+            _connections.Add(Context.User?.Identity?.Name, Context.ConnectionId);
+            await Clients.Client(Context.ConnectionId).ReceiveNotification($"The {Context.User?.Identity?.Name} ({Context.ConnectionId}) connected success!");
+            Console.WriteLine(Context.ConnectionId + "Is connect to hub!");
             base.OnConnectedAsync();
+            
 
         }
-
-        public async Task SendNotifications(string message)
+        public override Task OnDisconnectedAsync(Exception? exception)
         {
-            //await Clients.Client(Context.ConnectionId).ReceiveNotification(message);
-            //await Clients.All.ReceiveNotification($"{Context.ConnectionId} :{message}");
+            _connections.Remove(Context.User.Identity.Name, Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
         }
 
+        public void OnReconnected()
+        {
+
+                if (!_connections.GetConnections(Context.User.Identity.Name).Contains(Context.ConnectionId))
+                {
+                    _connections.Add(Context.User.Identity.Name, Context.ConnectionId);
+                }
+
+        }
+        //===================================================================================== Notify For Admin/Staff's Actions ===================================================================================== 
+        public async Task PushNotifyToAllUsers(string sender, string content, string url)
+        {
+
+            string msg = sender + _configuration["MessageContents:Common-004"] + content;
+            await Clients.All.ReceiveNotification(msg, url);
+            
+        }
+
+        public async Task PushNotifyToListUsers(string sender, string [] receiver, string content, string url)
+        {
+            string msg = sender + _configuration["MessageContents:Common-004"] + content;
+            await Clients.Users(receiver).ReceiveNotification(msg, url);
+
+        }
+
+        public async Task PushNotifyToUser(string sender, string receiver, string content, string url)
+        {
+            string msg = sender + _configuration["MessageContents:Common-004"] + content;
+            await Clients.User(receiver).ReceiveNotification(msg, url);
+
+        }
+        //===================================================================================== Notify For Member's Actions ===================================================================================== 
+        public async Task SendAddFriendReqNotify(string sender, string receiver, string url)
+        {
+            string msg = sender + _configuration["MessageContents:User-001"];
+            await Clients.User(receiver).ReceiveNotification(msg, url);
+
+        }
 
 
     }
