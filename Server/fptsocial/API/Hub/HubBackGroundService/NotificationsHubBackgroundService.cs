be18d38e3e6a94b566a0347810a8b97a2424061a
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-30 17:17:35 +0700

    [Add] Add Hub Background

--- Server/fptsocial/API/Hub/HubBackGroundService/NotificationsHubBackgroundService.cs
+++ Server/fptsocial/API/Hub/HubBackGroundService/NotificationsHubBackgroundService.cs
@@ -20,7 +20,7 @@ namespace API.Hub
         NORMAL,
         IMPORTANCE
     }
-
+    
     public class NotificationsHubBackgroundService : BackgroundService, INotificationsHubBackgroundService
     {
         const string NORMAL = "Normal";
@@ -152,7 +152,7 @@ namespace API.Hub
                 DateTime datetime = DateTime.Now;
                 _logger.LogInformation($"excute {1}", nameof(NotificationsHubBackgroundService), datetime);
 
-
+                
             }
         }
         public override Task StartAsync(CancellationToken cancellationToken)
