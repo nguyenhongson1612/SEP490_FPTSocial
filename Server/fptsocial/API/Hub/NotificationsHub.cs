39c1a75e02d60ec0b0af32c735a226f4ca2014b5
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-30 17:20:11 +0700

    [Add/Modify] Add SQLTableDependency and Modify Hub

--- Server/fptsocial/API/Hub/NotificationsHub.cs
+++ Server/fptsocial/API/Hub/NotificationsHub.cs
@@ -1,63 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using API.Hub;
+using Application.Commands.CreateNotifications;
 using Application.Hub;
+using Application.Queries.GetNotifications;
+using Domain.Enums;
+using Domain.Exceptions;
+using Domain.QueryModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
 
 namespace Application.Hub
 {
+    public enum NotificationsTypeEnum
+    {
+        NORMAL,
+        IMPORTANCE
+    }
+    
     public class NotificationsHub : Hub<INotificationsClient>
     {
         private readonly static ConnectionMapping<string> _connections =
             new ConnectionMapping<string>();
-        
-        private readonly IConfiguration _configuration; 
-        //private readonly HubConnection _hubConnection;
 
-        public NotificationsHub(IConfiguration configuration)
+        HttpContext _httpContext;
+        private readonly IConfiguration _configuration;
+        public readonly ICreateNotifications _createNotifications;
+        public readonly INotificationsHubBackgroundService _INotificationsHubBackgroundService;
+        
+        public NotificationsHub(IConfiguration configuration, INotificationsHubBackgroundService notificationsHubBackgroundService, ICreateNotifications createNotifications)
         {
             _configuration = configuration;
-            //_hubConnection = hubConnection;
+            _INotificationsHubBackgroundService = notificationsHubBackgroundService;
+            _createNotifications = createNotifications;
         }
+
         public override async Task OnConnectedAsync()
         {
-            _connections.Add(Context.User?.Identity?.Name, Context.ConnectionId);
-            await Clients.Client(Context.ConnectionId).ReceiveNotification($"The {Context.User?.Identity?.Name} ({Context.ConnectionId}) connected success!");
-            Console.WriteLine(Context.ConnectionId + "Is connect to hub!");
-            base.OnConnectedAsync();
-            
+            //_connections.Add(Context.User?.Identity?.Name, Context.ConnectionId);
+            //await Clients.Client(Context.ConnectionId).ReceiveNotification($"The {Context.User?.Identity?.Name} ({Context.ConnectionId}) connected success!");
+                await Clients.Client(Context.ConnectionId).ReceiveNotification($"The  ({Context.ConnectionId}) connected success!");
 
         }
-        public override Task OnDisconnectedAsync(Exception? exception)
+        public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            _connections.Remove(Context.User.Identity.Name, Context.ConnectionId);
-            return base.OnDisconnectedAsync(exception);
+            //_connections.Remove(Context.User.Identity.Name, Context.ConnectionId);
+            await Clients.Client(Context.ConnectionId).ReceiveNotification($"The  ({Context.ConnectionId}) disconnected !");
+           // return base.OnDisconnectedAsync(exception);
         }
 
         public void OnReconnected()
         {
 
-                if (!_connections.GetConnections(Context.User.Identity.Name).Contains(Context.ConnectionId))
-                {
-                    _connections.Add(Context.User.Identity.Name, Context.ConnectionId);
-                }
+            if (!_connections.GetConnections(Context.User.Identity.Name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(Context.User.Identity.Name, Context.ConnectionId);
+            }
 
         }
+
         //===================================================================================== Notify For Admin/Staff's Actions ===================================================================================== 
+
         public async Task PushNotifyToAllUsers(string sender, string content, string url)
         {
 
             string msg = sender + _configuration["MessageContents:Common-004"] + content;
             await Clients.All.ReceiveNotification(msg, url);
-            
+
         }
 
-        public async Task PushNotifyToListUsers(string sender, string [] receiver, string content, string url)
+        public async Task PushNotifyToListUsers(string sender, string[] receiver, string content, string url)
         {
             string msg = sender + _configuration["MessageContents:Common-004"] + content;
             await Clients.Users(receiver).ReceiveNotification(msg, url);
@@ -71,13 +92,20 @@ namespace Application.Hub
 
         }
         //===================================================================================== Notify For Member's Actions ===================================================================================== 
-        public async Task SendAddFriendReqNotify(string sender, string receiver, string url)
+
+        public async Task SendReactNotify(string notice)
         {
-            string msg = sender + _configuration["MessageContents:User-001"];
-            await Clients.User(receiver).ReceiveNotification(msg, url);
+
+            await _INotificationsHubBackgroundService.SendReactNotifyService(Context ,notice);
 
         }
 
+        public async Task PushAllNotifyByUserIdWithTableDependency(string userId)
+        {
+
+            await _INotificationsHubBackgroundService.PushAllNotifyByUserIdWithTableDependencyService(Context, userId);
+
+        }
 
     }
 
---
+++ Server/fptsocial/API/Hub/SubscribeSqlTableDependencies/SubscribeNotificationsTableDependency.cs
@@ -0,0 +1,41 @@
+﻿using Application.Hub;
+using Domain.CommandModels;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base;
+
+namespace API.Hub.SubscribeSqlTableDependencies
+{
+    public class SubscribeNotificationsTableDependency : ISubscribeSqlTableDependency
+    {
+        SqlTableDependency<Notification> _notificationCommandTableDependency;
+        NotificationsHub _notificationsHub;
+
+        public SubscribeNotificationsTableDependency(NotificationsHub notificationsHub)
+        {
+            _notificationsHub = notificationsHub;
+           
+        }
+
+        public void SubscribeTableDependency(string connectionString)
+        {
+            _notificationCommandTableDependency = new SqlTableDependency<Notification>(connectionString);
+            _notificationCommandTableDependency.OnChanged += TableDependency_OnChanged;
+            _notificationCommandTableDependency.OnError += TableDependency_OnError;
+            _notificationCommandTableDependency.Start();
+        }
+
+        private async void TableDependency_OnChanged(object sender, TableDependency.SqlClient.Base.EventArgs.RecordChangedEventArgs<Notification> e)
+        {
+            string userId = e.Entity.UserId.ToString();
+            if (e.ChangeType != TableDependency.SqlClient.Base.Enums.ChangeType.None)
+            {
+                await _notificationsHub.PushAllNotifyByUserIdWithTableDependency(userId);
+            }
+        }
+
+        private void TableDependency_OnError(object sender, TableDependency.SqlClient.Base.EventArgs.ErrorEventArgs e)
+        {
+            Console.WriteLine($"{nameof(Notification)} SqlTableDependency error: {e.Error.Message}");
+        }
+    }
+}
