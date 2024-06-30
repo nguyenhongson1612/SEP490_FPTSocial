810f1f105475cda1dc3052abd3623910d5835d3a
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-30 17:14:48 +0700

    [Add] GetNotificatrion - DB Interactions

---
+++ Server/fptsocial/Application/Queries/GetNotifications/IGetNotifications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetNotifications
+{
+    public interface IGetNotifications
+    {
+        public Domain.QueryModels.UserProfile GetUserProfileByUserId(string userId);
+        public List<Domain.QueryModels.Notification> GetNotifyByUserId(string userId);
+        public List<Domain.QueryModels.Notification> GetNotifyBySenderId(string senderId);
+    }
+}
