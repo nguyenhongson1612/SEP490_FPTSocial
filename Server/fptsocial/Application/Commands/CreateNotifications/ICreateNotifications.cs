9bd2fda05d180d6045485faa820bea186f220cf6
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-06-30 17:14:27 +0700

    [Add] CreateNotificatrion - DB Interactions

---
+++ Server/fptsocial/Application/Commands/CreateNotifications/ICreateNotifications.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.CreateNotifications
+{
+    public interface ICreateNotifications
+    {
+        public Task CreateNotitfication(string senderId, string receiverId, string notiMessage, string notifiUrl, [Optional] NotificationsTypeEnum? notificationsTypeEnum);
+    }
+}
