61739533d7b1ff7663c12ccef67e35acf7142958
Khouse169 <nguyenkhaihoandtd@gmail.com>
2024-07-03 15:47:23 +0700

    Create group post function

---
+++ Server/fptsocial/Application/Commands/CreateGroupPost/CreateGroupPostCommandResult.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+using Domain.CommandModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.CreateGroupPost
+{
+    public class CreateGroupPostCommandResult
+    {
+        public string GroupPostId { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public string? Content { get; set; }
+        public string? GroupPostNumber { get; set; }
+        public string GroupStatusId { get; set; } = null!;
+        public bool? IsHide { get; set; }
+        public List<CheckingBadWord.BannedWord> BannedWords { get; set; } = new List<CheckingBadWord.BannedWord>();
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
