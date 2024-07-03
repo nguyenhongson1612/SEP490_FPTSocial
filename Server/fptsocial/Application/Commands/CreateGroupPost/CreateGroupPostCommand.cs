f9b15c690a5317052af03ede8511ed8f028fba90
Khouse169 <nguyenkhaihoandtd@gmail.com>
2024-07-03 15:47:23 +0700

    Create group post function

---
+++ Server/fptsocial/Application/Commands/CreateGroupPost/CreateGroupPostCommand.cs
@@ -0,0 +1,21 @@
+﻿using Core.CQRS.Command;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Application.Commands.CreateGroupPost
+{
+    public class CreateGroupPostCommand : ICommand<CreateGroupPostCommandResult>
+    {
+       public Guid UserId { get; set; }
+       public string Content { get; set; }
+       public Guid GroupStatusId { get; set; }
+        public IEnumerable<string>? Photos { get; set; }
+        public IEnumerable<string>? Videos { get; set; }
+
+    }
+}
