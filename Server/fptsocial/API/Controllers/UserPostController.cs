ac938a15f6cc628df6e1f2d6fd53c03a90dc44ae
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-07-05 08:25:10 +0700

    Update getotheruserpost API

--- Server/fptsocial/API/Controllers/UserPostController.cs
+++ Server/fptsocial/API/Controllers/UserPostController.cs
@@ -56,6 +56,18 @@ namespace API.Controllers
         [Route("getotheruserpost")]
         public async Task<IActionResult> GetOtherUserPost([FromQuery]GetOtherUserPostQuery input)
         {
+            var rawToken = HttpContext.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
+            if (string.IsNullOrEmpty(rawToken))
+            {
+                return BadRequest();
+            }
+            var handle = new JwtSecurityTokenHandler();
+            var jsontoken = handle.ReadToken(rawToken) as JwtSecurityToken;
+            if (jsontoken == null)
+            {
+                return BadRequest();
+            }
+            input.UserId = Guid.Parse(jsontoken.Claims.FirstOrDefault(claim => claim.Type == "userId").Value);
             var res = await _sender.Send(input);
             return Success(res.Value);
         }
