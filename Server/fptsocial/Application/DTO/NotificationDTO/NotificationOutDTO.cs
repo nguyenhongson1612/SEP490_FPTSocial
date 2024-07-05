4bc80afda49567782d0973d4bc31de209aacfe8a
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-07-05 09:16:21 +0700

    [Add] Output DTO For Notifications

---
+++ Server/fptsocial/Application/DTO/NotificationDTO/NotificationOutDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.NotificationDTO
+{
+    public class NotificationOutDTO
+    {
+        public string SenderId { get; set; }
+        public string ReciverId { get; set; }
+        public string SenderAvatar { get; set; }
+        public string Message { get; set; }
+        public string Url { get; set; }
+    }
+}
