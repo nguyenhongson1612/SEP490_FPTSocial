c77f0d747fafcfb667ba9a7219fd4be8a3484e69
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-07-10 20:08:03 +0700

    Implement getUserpostvideo API

---
+++ Server/fptsocial/Application/Queries/GetUserPostVideo/GetUserPostVideoHandler.cs
@@ -0,0 +1,69 @@
+﻿using Application.DTO.GetUserProfileDTO;
+using Application.DTO.UserPostVideoDTO;
+using Application.Queries.GetUserPostVideo;
+using AutoMapper;
+using Core.CQRS;
+using Core.CQRS.Query;
+using Domain.Enums;
+using Domain.Exceptions;
+using Domain.QueryModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetUserPostVideo
+{
+    public class GetUserPostVideoHandler : IQueryHandler<GetUserPostVideoQuery, GetUserPostVideoResult>
+    {
+        private readonly fptforumQueryContext _context;
+        private readonly IMapper _mapper;
+
+        public GetUserPostVideoHandler(fptforumQueryContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<GetUserPostVideoResult>> Handle(GetUserPostVideoQuery request, CancellationToken cancellationToken)
+        {
+            if (_context == null)
+            {
+                throw new ErrorException(StatusCodeEnum.Context_Not_Found);
+            }
+
+            if (request == null)
+            {
+                return Result<GetUserPostVideoResult>.Failure("Request is null");
+            }
+
+            var post = await _context.UserPostVideos
+                .Include(x => x.UserStatus)
+                .Include(x => x.Video)
+                .FirstOrDefaultAsync(x => x.UserPostVideoId == request.UserPostVideoId);
+
+            var result = new GetUserPostVideoResult
+            {
+                UserPostVideoId = post.UserPostVideoId,
+                UserPostId = post.UserPostId,
+                VideoId = post.VideoId,
+                Content = post.Content,
+                UserPostVideoNumber = post.UserPostVideoNumber,
+                Status = new GetUserStatusDTO
+                {
+                    UserStatusId = post.UserStatusId,
+                    UserStatusName = _context.UserStatuses.Where(x => x.UserStatusId == post.UserStatusId).Select(x => x.StatusName).FirstOrDefault()
+                },
+                IsHide = post.IsHide,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                PostPosition = post.PostPosition,
+                Video = _mapper.Map<VideoDTO>(post.Video)
+            };
+
+            return Result<GetUserPostVideoResult>.Success(result);
+        }
+    }
+}
---
+++ Server/fptsocial/Application/Queries/GetUserPostVideo/GetUserPostVideoResult.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTO.GetUserProfileDTO;
+using Application.DTO.UserPostVideoDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetUserPostVideo
+{
+    public class GetUserPostVideoResult
+    {
+        public Guid UserPostVideoId { get; set; }
+        public Guid UserPostId { get; set; }
+        public Guid VideoId { get; set; }
+        public string? Content { get; set; }
+        public string? UserPostVideoNumber { get; set; }
+        public virtual GetUserStatusDTO? Status { get; set; }
+        public bool? IsHide { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? PostPosition { get; set; }
+
+        public virtual VideoDTO? Video { get; set; }
+    }
+}
