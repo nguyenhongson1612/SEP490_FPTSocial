e867a08fbe14806eb5fa857340bd3625be0a6986
DESKTOP-0FV784U\admin <quannahe164025@fpt.edu.vn>
2024-07-14 15:42:39 +0700

    Get Friend By Name

---
+++ Server/fptsocial/Application/Queries/GetFriendyName/GetFriendByNameQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTO.FriendDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetFriendyName
+{
+    public class GetFriendByNameQueryResult
+    {
+        public GetFriendByNameQueryResult()
+        {
+            getFriendByName = new List<GetAllFriendDTO>();
+        }
+        public List<GetAllFriendDTO> getFriendByName { get; set; }
+        public int Count { get; set; }
+    }
+}
