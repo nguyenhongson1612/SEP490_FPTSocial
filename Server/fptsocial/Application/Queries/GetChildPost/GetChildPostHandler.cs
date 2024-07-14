fb397cdd097c4902976991ddf27a5d527e8b8d41
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-07-14 12:54:52 +0700

    Implement getChildPost API and Delete getUserPostPhoto,Video API

---
+++ Server/fptsocial/Application/Queries/GetChildPost/GetChildPostHandler.cs
@@ -0,0 +1,152 @@
+﻿using Application.DTO.GetUserProfileDTO;
+using Application.DTO.UserPostPhotoDTO;
+using Application.DTO.UserPostVideoDTO;
+using AutoMapper;
+using Core.CQRS;
+using Core.CQRS.Query;
+using Domain.Enums;
+using Domain.Exceptions;
+using Domain.QueryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetChildPost
+{
+    public class GetChildPostHandler : IQueryHandler<GetChildPostQuery, GetChildPostResult>
+    {
+        private readonly fptforumQueryContext _context;
+        private readonly IMapper _mapper;
+
+        public GetChildPostHandler(fptforumQueryContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<GetChildPostResult>> Handle(GetChildPostQuery request, CancellationToken cancellationToken)
+        {
+            if (_context == null)
+            {
+                throw new ErrorException(StatusCodeEnum.Context_Not_Found);
+            }
+
+            if (request == null) {
+                throw new ErrorException(StatusCodeEnum.RQ01_Request_Is_Null);
+            }
+
+            var userPostId = await _context.UserPostPhotos
+                                            .Where(x => x.UserPostPhotoId == request.UserPostMediaId)
+                                            .Select(x => x.UserPostId)
+                                            .FirstOrDefaultAsync();
+            if(userPostId == Guid.Empty)
+            {
+                userPostId = await _context.UserPostVideos
+                                            .Where(x => x.UserPostVideoId == request.UserPostMediaId)
+                                            .Select(x => x.UserPostId)
+                                            .FirstOrDefaultAsync();
+            }
+
+            var combinedResults = new List<GetChildPostResult>();
+
+            var userPostPhoto = await _context.UserPostPhotos
+                .Include(x => x.UserPost)
+                .Include(x => x.UserStatus)
+                .Include(x => x.Photo)
+                .Where(x => x.UserPostId == userPostId)
+                .ToListAsync(cancellationToken);
+
+            foreach (var photo in userPostPhoto)
+            {
+                var user = await _context.UserProfiles.FirstOrDefaultAsync(x => x.UserId == photo.UserPost.UserId);
+                var avt = await _context.AvataPhotos.FirstOrDefaultAsync(x => x.UserId == photo.UserPost.UserId && x.IsUsed == true);
+                combinedResults.Add(new GetChildPostResult
+                {
+                    UserPostMediaId = photo.UserPostPhotoId,
+                    UserPostId = photo.UserPostId,
+                    MediaId = photo.PhotoId,
+                    Content = photo.Content,
+                    UserPostMediaNumber = photo.UserPostPhotoNumber,
+                    Status = new GetUserStatusDTO
+                    {
+                        UserStatusId = photo.UserStatusId,
+                        UserStatusName = _context.UserStatuses.Where(x => x.UserStatusId == photo.UserStatusId).Select(x => x.StatusName).FirstOrDefault()
+                    },
+                    IsHide = photo.IsHide,
+                    CreatedAt = photo.CreatedAt,
+                    UpdatedAt = photo.UpdatedAt,
+                    PostPosition = photo.PostPosition,
+                    MediaType = "Photo",
+                    Photo = _mapper.Map<PhotoDTO>(photo.Photo),
+                    UserId = user.UserId,
+                    FullName = user.FirstName + " " + user.LastName,
+                    Avatar = _mapper.Map<GetUserAvatar>(avt)
+                });
+            }
+
+            var userPostVideo = await _context.UserPostVideos
+                .Include(x => x.UserPost)
+                .Include(x => x.UserStatus)
+                .Include(x => x.Video)
+                .Where(x => x.UserPostId == userPostId)
+                .ToListAsync(cancellationToken);
+
+            foreach (var video in userPostVideo)
+            {
+                var user = await _context.UserProfiles.FirstOrDefaultAsync(x => x.UserId == video.UserPost.UserId);
+                var avt = await _context.AvataPhotos.FirstOrDefaultAsync(x => x.UserId == video.UserPost.UserId && x.IsUsed == true);
+                combinedResults.Add(new GetChildPostResult
+                {
+                    UserPostMediaId = video.UserPostVideoId,
+                    UserPostId = video.UserPostId,
+                    MediaId = video.VideoId,
+                    Content = video.Content,
+                    UserPostMediaNumber = video.UserPostVideoNumber,
+                    Status = new GetUserStatusDTO
+                    {
+                        UserStatusId = video.UserStatusId,
+                        UserStatusName = _context.UserStatuses.Where(x => x.UserStatusId == video.UserStatusId).Select(x => x.StatusName).FirstOrDefault()
+                    },
+                    IsHide = video.IsHide,
+                    CreatedAt = video.CreatedAt,
+                    UpdatedAt = video.UpdatedAt,
+                    PostPosition = video.PostPosition,
+                    MediaType = "Video",
+                    Video = _mapper.Map<VideoDTO>(video.Video),
+                    UserId = user.UserId,
+                    FullName = user.FirstName + " " + user.LastName,
+                    Avatar = _mapper.Map<GetUserAvatar>(avt)
+                });
+            }
+
+            combinedResults = combinedResults.OrderBy(x => x.PostPosition).ToList();
+            for (int i = 0; i < combinedResults.Count; i++)
+            {
+                if (i > 0)
+                {
+                    combinedResults[i].PreviousId = combinedResults[i - 1].UserPostMediaId;
+                }
+                else
+                {
+                    combinedResults[i].PreviousId = combinedResults[combinedResults.Count - 1].UserPostMediaId;
+                }
+
+                if (i < combinedResults.Count - 1)
+                {
+                    combinedResults[i].NextId = combinedResults[i + 1].UserPostMediaId;
+                }
+                else
+                {
+                    combinedResults[i].NextId = combinedResults[0].UserPostMediaId;
+                }
+            }
+
+            var result = combinedResults.FirstOrDefault(x => x.UserPostMediaId == request.UserPostMediaId);
+            return Result<GetChildPostResult>.Success(result);
+        }
+    }
+}
--- Server/fptsocial/Application/Queries/GetChildPost/GetChildPostResult.cs
+++ Server/fptsocial/Application/Queries/GetChildPost/GetChildPostResult.cs
@@ -1,4 +1,5 @@
 ﻿using Application.DTO.GetUserProfileDTO;
+using Application.DTO.UserPostPhotoDTO;
 using Application.DTO.UserPostVideoDTO;
 using System;
 using System.Collections.Generic;
@@ -6,25 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Queries.GetUserPostVideo
+namespace Application.Queries.GetChildPost
 {
-    public class GetUserPostVideoResult
+    public class GetChildPostResult
     {
-        public Guid UserPostVideoId { get; set; }
+        public Guid UserPostMediaId { get; set; }
         public Guid UserPostId { get; set; }
-        public Guid VideoId { get; set; }
+        public Guid MediaId { get; set; }
         public string? Content { get; set; }
-        public string? UserPostVideoNumber { get; set; }
-        public virtual GetUserStatusDTO? Status { get; set; }
+        public string? UserPostMediaNumber { get; set; }
+        public GetUserStatusDTO? Status { get; set; }
         public bool? IsHide { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public int? PostPosition { get; set; }
+        public string MediaType { get; set; }
 
-        public virtual VideoDTO? Video { get; set; }
+        public PhotoDTO? Photo { get; set; }
+        public VideoDTO? Video { get; set; }
 
         public Guid UserId { get; set; }
         public string? FullName { get; set; }
         public GetUserAvatar Avatar { get; set; }
+
+        public Guid? PreviousId { get; set; }
+        public Guid? NextId { get; set; }
     }
 }
