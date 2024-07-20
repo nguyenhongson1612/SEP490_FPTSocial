9528e3abb83420f6e708c7ef1371cffe2b0313dd
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-07-20 20:58:45 +0700

    Implement GetBannedPostByUserId API

---
+++ Server/fptsocial/Application/Queries/GetBannedPostByUserId/GetBannedPostByUserIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using Core.CQRS.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetBannedPostByUserId
+{
+    public class GetBannedPostByUserIdQuery : IQuery<List<GetBannedPostByUserIdResult>>
+    {
+        public Guid UserId { get; set; }
+    }
+}
