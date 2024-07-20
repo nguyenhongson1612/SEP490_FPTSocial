d1b4c70e9deeba81a64df783f7478f8a73aebba6
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-07-20 15:29:51 +0700

    Implement getReportType API and update GetPost API

---
+++ Server/fptsocial/Application/Queries/GetReportType/GetReportTypeResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetReportType
+{
+    public class GetReportTypeResult
+    {
+        public Guid ReportTypeId { get; set; }
+        public string ReportTypeName { get; set; } = null!;
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
