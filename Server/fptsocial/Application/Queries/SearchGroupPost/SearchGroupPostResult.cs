f6fce64d15ee4f8ffbc3c0a203ab8f4c55a10757
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-07-22 13:53:05 +0700

    Implement SearchGroupPost API

---
+++ Server/fptsocial/Application/Queries/SearchGroupPost/SearchGroupPostResult.cs
@@ -0,0 +1,14 @@
+﻿using Application.Queries.GetGroupPostByGroupId;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.SearchGroupPost
+{
+    public class SearchGroupPostResult
+    {
+        public List<GetGroupPostByGroupIdResult>? GroupPost {  get; set; }
+    }
+}
