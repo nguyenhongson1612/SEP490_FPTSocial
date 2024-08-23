381fa4d86511791bc1e9d942ec05d88e0ff466d7
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-08-24 05:05:17 +0700

    [Modify] update send mail for block/update profile/create/activate

--- Server/fptsocial/API/Hub/SubscribeSqlTableDependencies/SubscribeNotificationsTableDependency.cs
+++ Server/fptsocial/API/Hub/SubscribeSqlTableDependencies/SubscribeNotificationsTableDependency.cs
@@ -1,19 +1,20 @@
 ﻿using Application.Hub;
 using Domain.CommandModels;
 using TableDependency.SqlClient;
-using TableDependency.SqlClient.Base;
 
 namespace API.Hub.SubscribeSqlTableDependencies
 {
     public class SubscribeNotificationsTableDependency : ISubscribeSqlTableDependency
     {
         SqlTableDependency<Notification> _notificationCommandTableDependency;
+        SqlTableDependency<UserProfile> _blockUserProfileTableDependency;
         NotificationsHub _notificationsHub;
+        NotificationsHubBackgroundService _notificationsHubBackgroundService;
 
-        public SubscribeNotificationsTableDependency(NotificationsHub notificationsHub)
+        public SubscribeNotificationsTableDependency(NotificationsHub notificationsHub, NotificationsHubBackgroundService notificationsHubBackgroundService)
         {
             _notificationsHub = notificationsHub;
-           
+            _notificationsHubBackgroundService = notificationsHubBackgroundService;
         }
 
         public void SubscribeTableDependency(string connectionString)
@@ -31,6 +32,48 @@ namespace API.Hub.SubscribeSqlTableDependencies
             {
                 await _notificationsHub.PushAllNotifyByUserIdWithTableDependency(userId);
             }
+        }
+        public void SubscribeBlockUserProfileTableDependency(string connectionString)
+        {
+            _blockUserProfileTableDependency = new SqlTableDependency<UserProfile>(connectionString);
+            _blockUserProfileTableDependency.OnChanged += BlockUserProfileTableDependency_OnChanged;
+            _blockUserProfileTableDependency.OnError += TableDependency_OnError;
+            _blockUserProfileTableDependency.Start();
+        }
+
+        private async void BlockUserProfileTableDependency_OnChanged(object sender, TableDependency.SqlClient.Base.EventArgs.RecordChangedEventArgs<UserProfile> e)
+        {
+            if (!e.Entity.IsActive)
+            {
+                var isActive = e.Entity.IsActive;
+                string userId = e.Entity.UserId.ToString();
+                if (e.ChangeType != TableDependency.SqlClient.Base.Enums.ChangeType.None)
+                {
+                    await _notificationsHubBackgroundService.SendEmailAsync(e.Entity.Email, isActive, e.Entity, false);
+                }
+
+            }
+
+            if (e.Entity.IsActive && e.ChangeType == TableDependency.SqlClient.Base.Enums.ChangeType.Insert)
+            {
+                var isActive = e.Entity.IsActive;
+                string userId = e.Entity.UserId.ToString();
+                if (e.ChangeType != TableDependency.SqlClient.Base.Enums.ChangeType.None)
+                {
+                    await _notificationsHubBackgroundService.SendEmailAsync(e.Entity.Email, isActive, e.Entity, true);
+                }
+            }
+
+            if (e.Entity.IsActive && e.ChangeType == TableDependency.SqlClient.Base.Enums.ChangeType.Update)
+            {
+                var isActive = e.Entity.IsActive;
+                string userId = e.Entity.UserId.ToString();
+                if (e.ChangeType != TableDependency.SqlClient.Base.Enums.ChangeType.None)
+                {
+                    await _notificationsHubBackgroundService.SendEmailAsync(e.Entity.Email, isActive, e.Entity, false);
+                }
+            }
+
         }
 
         private void TableDependency_OnError(object sender, TableDependency.SqlClient.Base.EventArgs.ErrorEventArgs e)
