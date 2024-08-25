f858cc5991717968a82015b01b5b35047823f03f
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-08-25 22:32:34 +0700

    Implement GetDataForAdmin API

---
+++ Server/fptsocial/Application/Queries/GetDataForAdmin/GetDataForAdminResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetDataForAdmin
+{
+    public class GetDataForAdminResult
+    {
+        public int? NumberOfUser {  get; set; }
+        public int? NumberOfActiveUser { get; set; }
+        public int? NumberOfInactiveUser { get; set; }
+        public int? NumberOfPost {  get; set; }
+        public int? NumberOfGroup { get; set; }
+    }
+}
