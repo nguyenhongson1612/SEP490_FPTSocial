2bfd6d0a8ba12a3e674a9b48c7efd94a3c0d50fb
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-08-25 22:59:41 +0700

    Update DeactiveUser API

--- Server/fptsocial/API/Controllers/UserProfileController.cs
+++ Server/fptsocial/API/Controllers/UserProfileController.cs
@@ -547,23 +547,6 @@ namespace API.Controllers
         [Route("deactiveUserByUserId")]
         public async Task<IActionResult> ActiveUserByUserId([FromQuery] DeactiveUserCommand command)
         {
-            var rawToken = HttpContext.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
-            if (string.IsNullOrEmpty(rawToken))
-            {
-                return BadRequest();
-            }
-            var handle = new JwtSecurityTokenHandler();
-            var jsontoken = handle.ReadToken(rawToken) as JwtSecurityToken;
-            if (jsontoken == null)
-            {
-                return BadRequest();
-            }
-            var uid = jsontoken.Claims.FirstOrDefault(claim => claim.Type == "userId").Value;
-            if (string.IsNullOrEmpty(uid))
-            {
-                return BadRequest();
-            }
-            command.UserId = Guid.Parse(uid);
             var res = await _sender.Send(command);
             return Success(res.Value);
         }
@@ -572,23 +555,6 @@ namespace API.Controllers
         [Route("activeUserByUserId")]
         public async Task<IActionResult> ActiveUserByUserId([FromQuery] ActiveUserCommand command)
         {
-            var rawToken = HttpContext.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
-            if (string.IsNullOrEmpty(rawToken))
-            {
-                return BadRequest();
-            }
-            var handle = new JwtSecurityTokenHandler();
-            var jsontoken = handle.ReadToken(rawToken) as JwtSecurityToken;
-            if (jsontoken == null)
-            {
-                return BadRequest();
-            }
-            var uid = jsontoken.Claims.FirstOrDefault(claim => claim.Type == "userId").Value;
-            if (string.IsNullOrEmpty(uid))
-            {
-                return BadRequest();
-            }
-            command.UserId = Guid.Parse(uid);
             var res = await _sender.Send(command);
             return Success(res.Value);
         }
