dc5d4e4fb23e2bfa42ac6fa98869456663fbfa89
20 - Bùi Quốc Anh K16-HL <anhbqhe163864@fpt.edu.vn>
2024-08-25 12:36:51 +0700

    [Modify] fixapinoti

--- Server/fptsocial/Application/Queries/GetUserNotificationsList/GetUserNotificationsListQueryResult.cs
+++ Server/fptsocial/Application/Queries/GetUserNotificationsList/GetUserNotificationsListQueryResult.cs
@@ -10,15 +10,13 @@ namespace Application.Queries.GetUserNotificationsList
     public class GetUserNotificationsListQueryResult
     {
         public Guid NotificationId { get; set; }
-        public Guid UserId { get; set; }
-        public Guid SenderId { get; set; }
+        public string SenderId { get; set; }
         public string SenderName { get; set; }
-        public Guid NotificationTypeId { get; set; }
-        public string? NotiMessage { get; set; }
-        public Guid UserStatusId { get; set; }
-        public bool? IsRead { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public string? NotifiUrl { get; set; }
+        public string SenderAvatar { get; set; }
+        public string Message { get; set; }
+        public string Url { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; }
 
     }
 }
