5b38801300e32fdee655d63b4f37946df05dca3d
nguyenhongson1612 <sonnhhe164042@fpt.edu.vn>
2024-08-26 11:53:55 +0700

    Implement GetAllUser & GetAllGroup API

---
+++ Server/fptsocial/Application/Queries/GetAllGroupForAdmin/GetAllGroupForAdminResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetAllGroupForAdmin
+{
+    public class GetAllGroupForAdminResult
+    {
+        public List<GetAllGroup>? result;
+        public int? totalPage;
+    }
+    
+    public class GetAllGroup
+    {
+        public string? GroupName {  get; set; }
+        public string? CoverImage { get; set; }
+        public int? NumberOfMember {  get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
---
+++ Server/fptsocial/Application/Queries/GetAllUserForAdmin/GetAllUserForAdminHandler.cs
@@ -0,0 +1,77 @@
+﻿using Core.CQRS;
+using Core.CQRS.Query;
+using Domain.Enums;
+using Domain.Exceptions;
+using Domain.QueryModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.GetAllUserForAdmin
+{
+    public class GetAllUserForAdminHandler : IQueryHandler<GetAllUserForAdmin, GetAllUserForAdminResult>
+    {
+        private readonly fptforumQueryContext _context;
+
+        public GetAllUserForAdminHandler(fptforumQueryContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<GetAllUserForAdminResult>> Handle(GetAllUserForAdmin request, CancellationToken cancellationToken)
+        {
+            if(_context == null)
+            {
+                throw new ErrorException(StatusCodeEnum.Context_Not_Found);
+            }
+
+            var userList = await _context.UserProfiles
+                .AsNoTracking()
+                .ToListAsync();
+
+            var allUserList = new List<GetAllUser>();
+
+            foreach(var profile in userList)
+            {
+                var user = new GetAllUser();
+                user.Email = profile.Email;
+                user.Name = profile.FullName;
+                var avt = await _context.AvataPhotos
+                    .AsNoTracking()
+                    .Where(x => x.UserId == profile.UserId && x.IsUsed == true)
+                    .FirstOrDefaultAsync();
+                
+                if( avt != null )
+                {
+                    user.AvatarUrl = avt.AvataPhotosUrl;
+                }
+
+                if (profile.IsActive)
+                {
+                    user.IsActive = true;
+                }
+                else 
+                {
+                    user.IsActive = false;
+                }
+                user.CreatedAt = profile.CreatedAt;
+
+                allUserList.Add(user);   
+            }
+
+            var result = new GetAllUserForAdminResult
+            {
+                result = allUserList.OrderByDescending(x => x.CreatedAt)
+                                    .Skip((request.Page - 1) * request.PageSize)
+                                    .Take(request.PageSize)
+                                    .ToList(),
+                totalPage = (int)Math.Ceiling((double)allUserList.Count() / request.PageSize),
+            };
+
+            return Result<GetAllUserForAdminResult>.Success(result);
+        }
+    }
+}
